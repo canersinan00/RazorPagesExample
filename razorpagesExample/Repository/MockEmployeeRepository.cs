ba40ba7e714feb70cbfff35b8e869bfc58032684
canersinan00 <canersinan1013@gmail.com>
2025-10-29 11:56:59 +0300

    Employee List Created and Added

--- razorpagesExample/Repository/MockEmployeeRepository.cs
+++ razorpagesExample/Repository/MockEmployeeRepository.cs
@@ -13,10 +13,11 @@ public class MockEmployeeRepository : IEmployeeRepository
             new Employees{Id = 1, Name="Caner Sinan", Email="canersinan@live.com",Photo="1.jpg", Department="IT"},
             new Employees{Id = 2, Name="Berkay Özcan", Email="berkayozcan@live.com",Photo="2.jpg", Department="IT"},
             new Employees{Id = 3, Name="Samet Öztürk", Email="sametozturk@live.com",Photo="3.jpg", Department="IT"},
-            new Employees{Id = 4, Name="Batuhan Özcan", Email="batuhanozcan@live.com",Photo="4.jpg", Department="IT"},
-        }
+            new Employees{Id = 4, Name="Batuhan Özcan", Email="batuhanozcan@live.com",Photo="4.jpg", Department="IT"}
+        };
     }
     public IEnumerable<Employees> GetAll()
     {
         return _employeeList;
+    }
 }
