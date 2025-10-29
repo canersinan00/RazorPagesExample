68bb696cf0d29389ba269b141a5a2b89ccd53d2a
canersinan00 <canersinan1013@gmail.com>
2025-10-29 14:22:24 +0300

    Edit Page Create and Added

---
+++ razorpagesExample/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using razorpagesExample.Models;
+using razorpagesExample.Repository;
+
+namespace razorpagesExample.Pages;
+
+public class EditModel : PageModel
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EditModel(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public Employee Employee { get; set; } = null!;
+    public void OnGet(int id)
+    {
+        Employee = _employeeRepository.GetById(id);
+
+    }
+}
